b423538201cdb548d4d97ec12c3ca327fa1651d6
Youssef Ibrahim <Youssef.Ibrahim@microtec.com.eg>
2025-05-30 14:55:32 +0300

    init NGRX Store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore(), provideEffects()]
 };
